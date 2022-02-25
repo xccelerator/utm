a6c36bb152215eabfe97f857dfe207e13d33213d
xccelerator <cojocaridragos@gmail.com>
2022-02-25 21:01:46 +0200

    .

--- anul I/grafica pe calculator/gclab4.py
+++ anul I/grafica pe calculator/gclab4.py
@@ -1,3 +1,3 @@
 numar = int(input("Dati numarul:"))
 
-print("Numarul introdus de dumneavoastra are",len(str(numar)) if numar >= 1 else len(str(numar)) - 1,"cifre")
+print("Numarul introdus de dumneavoastra are",len(str(numar)) if numar >= 0 else len(str(numar)) - 1,"cifre")
