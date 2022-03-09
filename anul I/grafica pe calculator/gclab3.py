b45782f6654b6c89c9da1e0cc945b3f78cc2dd72
xccelerator <cojocaridragos@gmail.com>
2022-03-10 00:32:06 +0200

    .

--- anul I/grafica pe calculator/gclab3.py
+++ anul I/grafica pe calculator/gclab3.py
@@ -1,3 +1,4 @@
+print("*"*50)
 old_list = [[22,44,66],[11,33,55],[20,30,40]]
 print(old_list)
 
@@ -7,3 +8,28 @@ for i in range(len(old_list)):
     new_list.append(old_list[i][-1])
 
 print(new_list)
+
+print("*"*50)
+
+lista_mea = ['Vera', ['Ion', 'Vlad', 'Petru'], 'Ana', 'Ala'] 
+print(lista_mea[1][2]) 
+print(lista_mea[1]) 
+print(lista_mea[-1])
+
+print("*"*50)
+
+lista_mea = ['Vera', ['Ion', 'Vlad', 'Petru'], 'Ana', 'Ala']
+
+for i in lista_mea: 
+	print(len(i), end=" ") 
+
+lista_mea.append("Iulian") 
+
+print("\n",lista_mea) 
+
+lista_mea.pop(4); 
+lista_mea.insert(0,"Iulian") 
+
+print(lista_mea)
+
+print("*"*50)
